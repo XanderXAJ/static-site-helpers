9349995f59cd4c439dcf601a85e3b51c69fab175
Alex Palmer <xanderx@xanderx.com>
2018-04-20 21:30:47 +0100

    Initial commit -- lay out initial blueprint for script

---
+++ migrate_post_date.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import os
+
+
+import frontmatter
+
+
+def generate_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log_level', help='Set logging level', default='WARNING',
+                        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'])
+    parser.add_argument('paths', nargs='+', type=argparse.FileType('w'), metavar='path')
+    return parser
+
+
+def parse_arguments():
+    parser = generate_argument_parser()
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    for file in args.paths:
+        file_name = os.path.basename(file.name)
+        logging.info(file_name)
+        # TODO: Determine if date is in file name
+        # TODO: Extract date from file name
+        # TODO: Load post
+        # TODO: Add date to frontmatter
+        # TODO: Add lastmod to frontmatter
+        # TODO: Create file name without date
+        # TODO: Check file name without date doesn't already exist
+        # TODO: Write to file name without date
+        # TODO: Remove old file
+
+
+main()
