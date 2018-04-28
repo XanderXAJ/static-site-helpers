c5d0f59b39d74de3b65e9e318e570b553281f1c1
Alex Palmer <xanderx@xanderx.com>
2018-04-28 12:42:16 +0100

    Add tests for argument parsing

--- lib/arg.py
+++ lib/arg.py
@@ -1,14 +1,15 @@
 import argparse
 
 
-def generate_argument_parser():
-    parser = argparse.ArgumentParser()
+def generate_argument_parser(parser=None):
+    if parser is None:
+        parser = argparse.ArgumentParser()
     parser.add_argument('--log_level', help='Set logging level', default='WARNING',
                         choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'])
     parser.add_argument('paths', nargs='+', metavar='path')
     return parser
 
 
-def parse_arguments():
-    parser = generate_argument_parser()
-    return parser.parse_args()
+def parse_arguments(args=None, parser=None):
+    parser = generate_argument_parser(parser=parser)
+    return parser.parse_args(args)
---
+++ tests/lib/test_arg.py
@@ -0,0 +1,36 @@
+import argparse
+import unittest
+
+import lib.arg
+
+
+class TestException(Exception):
+    """Exception to be deliberately thrown as part of tests."""
+
+
+class RaiseArgumentParser(argparse.ArgumentParser):
+    """ArgumentParser that raises an exception instead of exiting the process."""
+
+    def error(self, message):
+        raise TestException(message)
+
+
+class TestArgIntegration(unittest.TestCase):
+    def test_no_path_fails(self):
+        with self.assertRaises(TestException):
+            lib.arg.parse_arguments([], parser=RaiseArgumentParser())
+
+    def test_single_path(self):
+        args = lib.arg.parse_arguments(['./file'])
+
+        self.assertEqual(args.paths, ['./file'])
+
+    def test_multiple_paths(self):
+        args = lib.arg.parse_arguments(['./file1', './file2'])
+
+        self.assertEqual(args.paths, ['./file1', './file2'])
+
+    def test_log_level_parsed(self):
+        args = lib.arg.parse_arguments(['--log_level', 'DEBUG', './file'])
+
+        self.assertEqual(args.log_level, 'DEBUG')
